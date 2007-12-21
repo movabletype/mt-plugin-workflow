cb35b548f522e5d134edafb630db58d33c9a9b8b
David Raynes <rayners@sixapart.com>
2007-12-21 21:33:05 +0000

    Bumped version number

--- plugins/Workflow/workflow.pl
+++ plugins/Workflow/workflow.pl
@@ -18,7 +18,7 @@ use Workflow::Step;
 use Workflow::StepAssociation;
 
 use vars qw($VERSION $plugin);
-$VERSION = '1.9';
+$VERSION = '1.9.1';
 $plugin = MT::Plugin::Workflow->new ({
         id          => 'Workflow',
         name		=> 'Workflow',
