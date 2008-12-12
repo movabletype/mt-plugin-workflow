4dfc3596d1a601ffcd07a79a95a39e1c92300d48
Kevin Shay <kshay@sixapart.com>
2008-12-12 02:31:21 +0000

    Version

--- plugins/Workflow/workflow.pl
+++ plugins/Workflow/workflow.pl
@@ -18,7 +18,7 @@ use Workflow::Step;
 use Workflow::StepAssociation;
 
 use vars qw($VERSION $plugin);
-$VERSION = '1.9.11';
+$VERSION = '1.9.12mt4-system';
 $plugin = MT::Plugin::Workflow->new ({
         id          => 'Workflow',
         name		=> 'Workflow',
